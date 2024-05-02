2827a76d7b71279c01f34bb43c1c9a0c74dd18a3
jshan94306 <47008278+jshan94306@users.noreply.github.com>
2024-05-02 11:59:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 
 
 
-<!-- document.addEventListener('DOMContentLoaded', function() {
-    var toggleLinks = document.querySelectorAll('.toggle-link');
-    toggleLinks.forEach(function(toggleLink) {
-        toggleLink.addEventListener('click', function(e) {
-            e.preventDefault();
-            var expandableContent = this.closest('.expandable-content');
-            var fullContent = expandableContent.querySelector('.full-content');
-            expandableContent.classList.toggle('expanded');
-            fullContent.style.display = expandableContent.classList.contains('expanded') ? 'block' : 'none';
-            var text = this.innerText === 'Show more' ? 'Show less' : 'Show more';
-            this.innerText = text;
-        });
-    });
-});
--->
-
-
 document.addEventListener('DOMContentLoaded', function() {
     var toggleLinks = document.querySelectorAll('.toggle-link');
     toggleLinks.forEach(function(toggleLink) {
