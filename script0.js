6056e80f0a5a9cb8f21b661c00e92bb4d63ccc3c
jshan94306 <47008278+jshan94306@users.noreply.github.com>
2024-05-02 22:52:18 -0700

    Update script0.js

--- script0.js
+++ script0.js
@@ -11,13 +11,37 @@
     });
 }); */
 
-document.addEventListener('DOMContentLoaded', function() {
+/* document.addEventListener('DOMContentLoaded', function() {
     var toggleLinks = document.querySelectorAll('.toggle-link');
     toggleLinks.forEach(function(toggleLink) {
         toggleLink.addEventListener('click', function(e) {
             e.preventDefault();
             var expandableContent = this.closest('.expandable-content');
             var fullContent = expandableContent.querySelector('.full-content');
+            expandableContent.classList.toggle('expanded');
+            fullContent.style.display = expandableContent.classList.contains('expanded') ? 'block' : 'none';
+            var text = this.innerText === 'Show more' ? 'Show less' : 'Show more';
+            this.innerText = text;
+        });
+    });
+}); */
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleLinks = document.querySelectorAll('.toggle-link');
+    toggleLinks.forEach(function(toggleLink) {
+        toggleLink.addEventListener('click', function(e) {
+            e.preventDefault();
+            var expandableContent = this.closest('.expandable-content');
+            var defaultContent = expandableContent.querySelector('.default-content');
+            var fullContent = expandableContent.querySelector('.full-content');
+            
+            if (expandableContent.classList.contains('expanded')) {
+                defaultContent.style.display = 'none';
+            } else {
+                defaultContent.style.display = 'block';
+            }
+
             expandableContent.classList.toggle('expanded');
             fullContent.style.display = expandableContent.classList.contains('expanded') ? 'block' : 'none';
             var text = this.innerText === 'Show more' ? 'Show less' : 'Show more';
