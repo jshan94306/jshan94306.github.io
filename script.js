9070fd38848907db631eec9d78393fa070fd7cdc
jshan94306 <47008278+jshan94306@users.noreply.github.com>
2024-05-02 18:22:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-
 document.addEventListener('DOMContentLoaded', function() {
+    var toggleLinks = document.querySelectorAll('.toggle-link');
+    toggleLinks.forEach(function(toggleLink) {
+        toggleLink.addEventListener('click', function(e) {
+            e.preventDefault();
+            var expandableContent = this.closest('.expandable-content');
+            var defaultContent = expandableContent.querySelector('.default-content');
+            var fullContent = expandableContent.querySelector('.full-content');
+            
+            var isExpanded = expandableContent.classList.toggle('expanded');
+            fullContent.style.display = isExpanded ? 'block' : 'none';
+            defaultContent.style.display = isExpanded ? 'none' : 'block';
+            
+            var text = isExpanded ? 'Show less' : 'Show more';
+            this.innerText = text;
+        });
+    });
+});
+
+<!-- document.addEventListener('DOMContentLoaded', function() {
     var toggleLinks = document.querySelectorAll('.toggle-link');
     toggleLinks.forEach(function(toggleLink) {
         toggleLink.addEventListener('click', function(e) {
@@ -16,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
             this.innerText = text;
         });
     });
-});   
+});   -->
 
 
 
