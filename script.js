29672889a572c98dac01db1c72a3c58753cd93c4
jshan94306 <47008278+jshan94306@users.noreply.github.com>
2024-05-02 17:40:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-
-<!--  document.addEventListener('DOMContentLoaded', function() {
-    var toggleLinks = document.querySelectorAll('.toggle-link');
-    toggleLinks.forEach(function(toggleLink) {
-        toggleLink.addEventListener('click', function(e) {
-            e.preventDefault();
-            var expandableContent = this.closest('.expandable-content');
-            var defaultContent = expandableContent.querySelector('.default-content');
-            var fullContent = expandableContent.querySelector('.full-content');
-            
-            var isExpanded = expandableContent.classList.toggle('expanded');
-            fullContent.style.display = isExpanded ? 'block' : 'none';
-            defaultContent.style.display = isExpanded ? 'none' : 'block';
-            
-            var text = isExpanded ? 'Show less' : 'Show more';
-            this.innerText = text;
-        });
-    });
-});   -->
+>
 
 
-
-<!-- The following version works but doesn't show Show less. -->
 document.addEventListener('DOMContentLoaded', function() {
     var toggleLinks = document.querySelectorAll('.toggle-link');
     toggleLinks.forEach(function(toggleLink) {
